73f8f4f89700eeadac3940a7e9445ee56863e87d
Adam Wong <wizzardcloud@gmail.com>
2022-05-11 20:28:52 +1000

    Add unit test codes

---
+++ scripts/run_tests.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+SCRIPTPATH="$(cd "$(dirname "$0")"; pwd -P)"
+
+cd $SCRIPTPATH
+cd ..
+
+source venv/bin/activate
+
+export PYTHONPATH="./fliton_fib_py"
+
+python -m unittest discover
